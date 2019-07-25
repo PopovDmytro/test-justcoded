7f9a7bbf1727bf3426e6aad41a9e81d46dc92fd8
PopovDmytro <dimahom@ukr.net>
2019-07-25 14:21:55 +0300

    init commit

---
+++ precache-manifest.a438a275666cd113d01492c9c2580fef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d0011cda110dd02bbdefbabcf9468e0",
+    "url": "/test-justcoded/index.html"
+  },
+  {
+    "revision": "3503c986321cbb56e211",
+    "url": "/test-justcoded/static/css/main.95337415.chunk.css"
+  },
+  {
+    "revision": "3f13b28665e8860e91ce",
+    "url": "/test-justcoded/static/js/2.18a0b9ad.chunk.js"
+  },
+  {
+    "revision": "3503c986321cbb56e211",
+    "url": "/test-justcoded/static/js/main.3c0b0196.chunk.js"
+  },
+  {
+    "revision": "4b0e2d5aba4955a90656",
+    "url": "/test-justcoded/static/js/runtime~main.1bc0101c.js"
+  }
+]);
